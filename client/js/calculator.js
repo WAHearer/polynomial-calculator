afa5b4fb4458f894e917770e03781fd4c7b5e3d0
Tianhe Zhang <62363213+WAHearer@users.noreply.github.com>
2024-09-18 19:56:01 +0800

    Update calculator.js

--- client/js/calculator.js
+++ client/js/calculator.js
@@ -1,5 +1,12 @@
 function calc(){
 	let polynomial=document.getElementById("input").value;
+	if(polynomial[0]==='?'){
+		alert("多项式不合法！");
+		return;
+	}
+	let polynomialForShow=polynomial;
+	let mode=document.getElementById("select").value;
+	polynomial=mode+polynomial;
 	var url="http://localhost:8080/calc";
 	fetch(url,{
 		method:"POST",
@@ -14,7 +21,7 @@ function calc(){
 		if(data=="invalid")
 			alert("多项式不合法！");
 		else
-			document.getElementById("result").innerHTML=polynomial+" = "+data;
+			document.getElementById("result").innerHTML=polynomialForShow+" = "+data;
 	}).catch(function(err){
 		console.log(err);
 	});
